dd627d72a1df1d6fed54763b4a4cf4dbac8663e8
Roshaan Butt <roshaan.zafar@theviewlytics.net>
2023-09-17 15:02:48 +0000

    changes for yahoo finance data

--- models/stock_market/staging/YAHOO_STOCKS_BATCH_DATA.sql
+++ models/stock_market/staging/YAHOO_STOCKS_BATCH_DATA.sql
@@ -1,19 +1,18 @@
---Set target properties
-{{ config(
-    database='DBT_STAGING',
-     tags=["stocks_batch"],
-    pre_hook="ALTER EXTERNAL TABLE DBT_RAW.LANDING_ZONE.BATCH_DATA_STOCKS REFRESH" 
-) }}
+-- Set target properties
+{{
+    config(
+        database="DBT_STAGING",
+        tags=["stocks_batch"],
+        pre_hook="ALTER EXTERNAL TABLE DBT_RAW.LANDING_ZONE.BATCH_DATA_STOCKS REFRESH",
+    )
+}}
 
-SELECT
-DATE
-,TICKER
-,ROUND(OPEN) AS OPEN
-,ROUND(HIGH) AS HIGH
-,ROUND(LOW) AS LOW
-,ROUND(CLOSE) AS CLOSE
-,VOLUME
-FROM
-DBT_RAW.LANDING_ZONE.BATCH_DATA_STOCKS
-
-
+select
+    date,
+    ticker,
+    round(open) as open,
+    round(high) as high,
+    round(low) as low,
+    round(close) as close,
+    volume
+from dbt_raw.landing_zone.batch_data_stocks
